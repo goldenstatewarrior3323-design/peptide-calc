b5ba4ae60d3666208f90286d891d706884b18843
goldenstatewarrior3323-design <goldenstatewarrior3323@gmail.com>
2026-06-18 22:43:38 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'peptide-calc-v1';
+const CACHE = 'peptide-calc-v2';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
