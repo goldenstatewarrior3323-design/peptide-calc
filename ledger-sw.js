8c46d6ce0e1c6504565b90e0a29395414669d2c2
goldenstatewarrior3323-design <goldenstatewarrior3323@gmail.com>
2026-07-13 09:34:05 -0700

    Add files via upload

---
+++ ledger-sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'monthly-ledger-v1';
+const URLS_TO_CACHE = ['monthly-ledger.html'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
